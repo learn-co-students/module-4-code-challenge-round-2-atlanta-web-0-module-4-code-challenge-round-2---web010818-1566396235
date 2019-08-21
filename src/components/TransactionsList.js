0edb0ca2d88840812cfcc284583b284d5e444a1b
Nerajno <Nerajno@gmail.com>
2019-08-21 10:52:51 -0400

    Either or iter.... good run..Thanks Ronnie

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import Transaction from "./Transaction";
 
-const TransactionsList = () => {
+const TransactionsList = (trans) => {
 
   return (
     <table className="ui celled striped padded table">
@@ -28,7 +29,9 @@ const TransactionsList = () => {
           </th>
         </tr>
 
-        {"... your code here..."}
+        {trans.filteredTransactions.map(eachTrans => {
+        return <Transaction trans={eachTrans}/>})
+      }
 
       </tbody>
     </table>
